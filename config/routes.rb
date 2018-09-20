524af64713163c811d2db63a9533962709867cea
Alex Calaca <alexcalaca@gmail.com>
2018-09-20 16:40:08 +0000

    Generate demo controller to test rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get "demo/index"  #Simple (matching) route
-  match "demo/index", :to => "demo#index", :via => :get # Longer version of a simple route
+  get 'demo/index'
+  get 'demo/hello'
+  #get "demo/index"  #Simple (matching) route
+  #match "demo/index", :to => "demo#index", :via => :get # Longer version of a simple route
 
 end
