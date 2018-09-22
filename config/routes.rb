bc1c94fd36269361dda4e05089fd7928b14814a1
Alex Calaca <alexcalaca@gmail.com>
2018-09-22 20:55:09 +0000

    Create hello and index view files for testing purposes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  
+  root 'demo#index'
   get 'demo/index'
   get 'demo/hello'
+  get 'demo/other_hello'
   #get "demo/index"  #Simple (matching) route
   #match "demo/index", :to => "demo#index", :via => :get # Longer version of a simple route
 
