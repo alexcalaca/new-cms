8bd9e7cfa9ae2e4b7fcbc3af66b25ca31787ff39
Alex Calaca <alexcalaca@gmail.com>
2018-09-25 02:32:02 +0000

    Add relationship to page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,5 @@
 class Page < ApplicationRecord
   has_and_belongs_to_many :admin_users, :join_table => 'admin_users_pages'
   belongs_to :subject, { :optional => false}
-  has_many :sections, { :optional => false}
+  has_many :sections
 end
