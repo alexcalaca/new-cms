3adc7d3c17c346ec7d0cead7b4c3e37b26fe26ee
Alex Calaca <alexcalaca@gmail.com>
2018-09-26 15:40:47 +0000

    Insert new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   
+  get 'subjects/index'
+  get 'subjects/show'
+  get 'subjects/new'
+  get 'subjects/edit'
+  get 'subjects/delete'
+
   root 'demo#index'
   get 'demo/index'
   get 'demo/hello'
   get 'demo/other_hello'
   #get "demo/index"  #Simple (matching) route
   #match "demo/index", :to => "demo#index", :via => :get # Longer version of a simple route
-
 end
