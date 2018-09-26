6ab2507b50ed7d410030b981e953b1cba943ff15
Alex Calaca <alexcalaca@gmail.com>
2018-09-26 15:39:22 +0000

    Create relationship with sections through section_edits

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,5 @@
 class AdminUser < ApplicationRecord
   has_and_belongs_to_many :pages, :join_table => 'admin_users_pages'
   has_many :section_edits
+  has_many :sections, :through => :section_edits
 end
