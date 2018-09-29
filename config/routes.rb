96cdfe8887d55196f5816a3b9bbb8804811c20c7
Alex Calaca <alexcalaca@gmail.com>
2018-09-29 14:05:14 +0000

    Include two resourceful routes for sections and pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,18 @@ Rails.application.routes.draw do
   
   root 'demo#index'
   
+  resources :sections do
+    member do
+      get :delete
+    end
+  end
+  
+  resources :pages do
+    member do
+      get :delete
+    end
+  end
+  
   resources :subjects do
     member do
       get :delete
