7c253a2735c9f2c864072c77d4e27162e5e619cf
Alex Calaca <alexcalaca@gmail.com>
2018-09-30 03:40:49 +0000

    Include named scope for page

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,4 +2,6 @@ class Page < ApplicationRecord
   has_and_belongs_to_many :admin_users, :join_table => 'admin_users_pages'
   belongs_to :subject, { :optional => false}
   has_many :sections
+  
+  scope :sorted, lambda { order("position ASC") }
 end
