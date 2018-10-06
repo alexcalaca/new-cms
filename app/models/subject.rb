d9117a289322c50453c431be582fe237d4c8a515
Alex Calaca <alexcalaca@gmail.com>
2018-10-06 20:59:46 +0000

    Define validation for subject model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,7 @@
 class Subject < ApplicationRecord
   has_many :pages
   
-  validates_presence_of :name
+#  validates_presence_of :name
+  validates :name, presence: true, length: { within: 4..55 }
+  validates :position, numericality: { only_integer: true }, allow_nil: false
 end
