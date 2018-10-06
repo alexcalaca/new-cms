64447103a19f25a5a863b9b926e6e1fa5f05027c
Alex Calaca <alexcalaca@gmail.com>
2018-10-06 20:58:42 +0000

    Define validations for admin user

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,4 +2,11 @@ class AdminUser < ApplicationRecord
   has_and_belongs_to_many :pages, :join_table => 'admin_users_pages'
   has_many :section_edits
   has_many :sections, :through => :section_edits
+  
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+  
+  validates :first_name, presence: true, length: { maximum: 25}
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { within: 8..25 }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 100 }, format: { with: EMAIL_REGEX }, confirmation: true
 end
