2bdbd1d2344051c0a2917551bc17d44612d80b9a
Alex Calaca <alexcalaca@gmail.com>
2018-10-06 20:59:04 +0000

    Define validations for page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -6,4 +6,7 @@ class Page < ApplicationRecord
   scope :sorted, lambda { order("position ASC ") }
   scope :page_sections, lambda { Section.order("position") }
   scope :page_count, lambda { Page.count }
+  
+  validates :name, presence: true, length: { within: 4..55 }
+  validates :permalink, presence: true, length: { within: 3..255 }, uniqueness: true
 end
