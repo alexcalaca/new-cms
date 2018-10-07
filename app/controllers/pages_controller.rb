7079b7a7f172f13e6da701349d39618299097664
Alex Calaca <alexcalaca@gmail.com>
2018-10-07 19:43:16 +0000

    Include controller filters in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   
   layout 'admin'
+  before_action :find_subjects, :only => [:new, :create, :edit, :update]
+  before_action :set_page_count, :only => [:new, :create, :edit, :update]
+  
+  
   def index
     @pages = Page.sorted
   end
@@ -12,16 +16,15 @@ class PagesController < ApplicationController
   def edit
     @page = Page.find(params[:id])
     @page_sections = Page.page_sections
-    @page_count = Page.page_count
   end
   
   def update
     @page = Page.find(params[:id])
     
     if @page.update_attributes(page_params)
+      flash[:notice] = "Page updated successfully"
       redirect_to(page_path(@page))
     else
-      @page_count = Page.page_count
       @page_sections = Page.page_sections
       render 'edit'
     end
@@ -30,16 +33,13 @@ class PagesController < ApplicationController
   def new
     @page = Page.new
     @page_sections = Page.page_sections
-    @page_count = Page.page_count + 1
   end
 
   def create
     @page = Page.new(page_params)
-    
     if @page.save
       redirect_to(pages_path) #It redirects to index
     else
-      @page_count = Page.page_count + 1
       @page_sections = Page.page_sections
       render('new')
     end
@@ -51,18 +51,25 @@ class PagesController < ApplicationController
   
   def destroy
     @page = Page.find(params[:id])
-    
-    if @page.destroy
-      redirect_to(pages_path)
-    else
-      
-    end
+    @page.destroy
+    flash[:notice] = "Flash destroyed successfully"
+    redirect_to(pages_path)
   end
   
   private
   def page_params
     params.require(:page).permit(:name, :permalink, :position, :visible, :subject_id)
   end
+  
+  def find_subjects
+    @pages = Page.sorted
+  end
+  
+  def set_page_count
+    @page_count = Page.count
+    if params[:action] == 'new' || params[:action] == 'create'
+      @page_count += 1
+    end
+  end
+  
 end
-
-
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -4,9 +4,20 @@ class AdminUser < ApplicationRecord
   has_many :sections, :through => :section_edits
   
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+  FORBIDDEN_NAMES = ['Jordan', 'test', 'mary']
   
   validates :first_name, presence: true, length: { maximum: 25}
   validates :last_name, presence: true, length: { maximum: 50 }
-  validates :username, presence: true, length: { within: 8..25 }, uniqueness: true
+  #validates :username, presence: true, length: { within: 8..25 }, uniqueness: true
   validates :email, presence: true, length: { maximum: 100 }, format: { with: EMAIL_REGEX }, confirmation: true
+  
+  validate :first_name_is_allowed
+  
+  private
+  
+  def first_name_is_allowed
+    if FORBIDDEN_NAMES.include?(first_name)
+      errors.add(:first_name, "has been restricted from use.")
+    end
+  end
 end
