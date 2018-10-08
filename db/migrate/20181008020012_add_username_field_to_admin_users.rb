ab0d480814ea80bc7dcd465988083b28a0120d60
Alex Calaca <alexcalaca@gmail.com>
2018-10-08 02:13:59 +0000

    Add username field to admin users

---
+++ db/migrate/20181008020012_add_username_field_to_admin_users.rb
@@ -0,0 +1,5 @@
+class AddUsernameFieldToAdminUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :admin_users, :username, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181008011314) do
+ActiveRecord::Schema.define(version: 20181008020012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20181008011314) do
     t.string   "password_digest", limit: 40
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
+    t.string   "username"
   end
 
   create_table "admin_users_pages", id: false, force: :cascade do |t|
