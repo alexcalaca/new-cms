e78a23aad913586d036cc1b07917b1df850a20b6
Alex Calaca <alexcalaca@gmail.com>
2018-10-08 02:14:22 +0000

    Change routes for access controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   
   root 'demo#index'
   
+  get 'admin', :to => 'access#menu'
+  get 'access/menu'
+  get 'access/login'
+  post 'access/attempt_login'
+  get 'access/logout'
+  
   resources :sections do
     member do
       get :delete
